3d5e9001a7f53b088ef949f9176fec5271d4d177
Caio Costa <caio-dot-costa-at-outlook-dot-com>
2014-04-03 22:49:02 -0300

    Criacao do projeto eclipse

---
+++ src/br/unb/cic/poo/controlefinancas/web/Main.java
@@ -0,0 +1,19 @@
+package br.unb.cic.poo.controlefinancas.web;
+
+import static spark.Spark.*;
+import spark.*;
+
+public class Main {
+
+   public static void main(String[] args) {
+      
+      get(new Route("/hello") {
+         @Override
+         public Object handle(Request request, Response response) {
+            return "Hello World!";
+         }
+      });
+
+   }
+
+}
